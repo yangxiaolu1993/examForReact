4e2f55e3a361fe21170340f40d78967a709856f9
yangxiaolu3 <yangxiaolu3@jd.com>
2020-11-19 13:59:51 +0800

    antd

--- src/pages/antd.js
+++ src/pages/antd.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react'
-import { TimePicker, Button,AutoComplete  } from 'antd';
+import { TimePicker, Button,AutoComplete ,Badge } from 'antd';
 import 'antd/dist/antd.css';
-
+import '../assets/antd.less'
+const { Option } = AutoComplete;
 class Antd extends React.Component {
   state = { 
     open: false,
-    options:[{value:'nihao'},{value:'jell'},{value:'AA'},{value:'nihao'},{value:'jell'},{value:'AA'},{value:'nihao'},{value:'jell'},{value:'AA'}]
+    options:[{value:'nihao',key:'nihao'},{value:'jell',key:'jell'},{value:'AA',key:'AA'},{value:'nihao'},{value:'jell'},{value:'AA'},{value:'nihao'},{value:'jell'},{value:'AA'}],
+    options1:[{value:'nihao',key:'nihao'},{value:'jell',key:'jell'},{value:'AA',key:'AA'}],
+    count:2
   };
   handleOpenChange = open => {
     this.setState({ open });
@@ -15,11 +18,13 @@ class Antd extends React.Component {
 
   componentDidMount(){
     
-    const se = new Set(['ll'])
-    console.log(se.has('l'))
-    
   }
-
+  add = ()=>{
+    const newCount = this.state.count+1
+    this.setState({
+      count:0
+    })
+  }
   render() {
     return (
       <div>
@@ -33,11 +38,34 @@ class Antd extends React.Component {
           )}
         />
 
-        <AutoComplete
-          options={this.state.options}
-          style={{ width: 200 }}
-          placeholder="control mode"
-        />
+        {/* <div className="badge">
+          <Badge count={this.state.count}>
+            <a href="#" className="head-example" />
+          </Badge>
+
+          <div onClick={this.add}>点击</div>
+        </div> */}
+
+        <div className="badge">
+          <AutoComplete
+            style={{ width: 200 }}
+            placeholder="control mode"
+            
+          >
+             <AutoComplete.Option key={'start'} value={'start'}>
+              <p>AutoComplete</p>
+            </AutoComplete.Option>
+            <AutoComplete.Option key={'京东'} value={'京东'}>
+              京东
+            </AutoComplete.Option>
+            <AutoComplete.Option key={'end'} value={'end'}>
+              ‘end‘
+            </AutoComplete.Option>
+          </AutoComplete>
+        </div>
+        
+        
+        
       </div>
     );
   }
